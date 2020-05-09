c7a465419ee2f68867fdaac8b888db5e7d202593
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2020-05-09 21:57:28 +0300

    add SendEmailAsync

--- src/Infrastructure/Services/EmailSender.cs
+++ src/Infrastructure/Services/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+﻿using MailKit.Net.Smtp;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using MimeKit;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Infrastructure.Services
@@ -7,10 +9,30 @@ namespace Microsoft.eShopWeb.Infrastructure.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
-            return Task.CompletedTask;
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Адміністрація", "pady@ukr.net"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.ukr.net", 465, true);
+                await client.AuthenticateAsync("pady@ukr.net", "JG8nbBbU248dtR9l");
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
         }
+        //public Task SendEmailAsync(string email, string subject, string message)
+        //{
+        //    // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
+        //    return Task.CompletedTask;
+        //}
     }
 }
