569968c699ce97332d5eaca2ca04db06394b0091
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2020-05-16 17:53:59 +0300

    Add Page CreateProduct

---
+++ src/Web/Pages/Admin/CreateProductItem.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.ApplicationCore.Constants;
+using Microsoft.eShopWeb.Web.ViewModels;
+
+namespace Microsoft.eShopWeb.Web.Pages.Admin
+{
+    [Authorize(Roles = AuthorizationConstants.Roles.ADMINISTRATORS)]
+    public class CreateProductItemModel : PageModel
+    {
+        [BindProperty]
+        public CatalogCreateViewModel CatalogCreateModel { get; set; } = new CatalogCreateViewModel();
+        public void OnGet()
+        {
+
+        }
+    }
+}
